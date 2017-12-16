71df38a81f985c4f0f2ccb7875780bbbde526c93
Ephraim Kunz <ephraimkunz@me.com>
2017-12-16 14:50:25 -0700

    Add ability to get basic github info

--- dailygithub.go
+++ dailygithub.go
@@ -3,43 +3,106 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 )
 
-type Test struct {
-	PersistChanges bool `json:"persistChanges,omitempty"`
+type User struct {
+	Name              string `json:"name,omitempty"`
+	PublicRepos       int    `json:"public_repos,omitempty"`
+	Followers         int    `json:"followers,omitempty"`
+	Following         int    `json:"following,omitempty"`
+	TotalPrivateRepos int    `json:"total_private_repos,omitempty"`
+	OwnedPrivateRepos int    `json:"owned_private_repos,omitempty"`
+}
+
+type FulfillmentReq struct {
+	OriginalRequest OriginalReq `json:"originalRequest,omitempty"`
+}
+
+type OriginalReq struct {
+	Data DataReq `json:"data,omitempty"`
+}
+
+type DataReq struct {
+	User UserReq `json:"user,omitempty"`
+}
+
+type FulfillmentResp struct {
+	Speech      string `json:"speech,omitempty"`
+	DisplayText string `json:"displayText,omitempty"`
+}
+
+type UserReq struct {
+	LastSeen    string `json:"lastSeen,omitempty"`
+	AccessToken string `json:"accessToken,omitempty"`
+	Locale      string `json:"locale,omitempty"`
+	UserId      string `json:"userId,omitempty"`
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-	log.Printf("Headers: %v", r.Header)
-
 	if r.Method == "POST" {
-		b, err := ioutil.ReadAll(r.Body)
-		defer r.Body.Close()
-		log.Printf("Body: %s", string(b))
+		decoder := json.NewDecoder(r.Body)
+		fulfillmentReq := &FulfillmentReq{}
+		err := decoder.Decode(fulfillmentReq)
+		if err != nil {
+			http.Error(w, "Error decoding json", http.StatusInternalServerError)
+		}
 
 		w.Header().Set("Content-Type", "application/json")
-		test := getResponse()
-		item, err := json.Marshal(test)
+		user, err := getCurrentUser(fulfillmentReq.OriginalRequest.Data.User.AccessToken)
+		if err != nil {
+			log.Println(err)
+		}
+
+		fulfillmentResp := buildFulfillment(user)
+		resp, err := json.Marshal(fulfillmentResp)
 		if err != nil {
 			http.Error(w, "Error marshaling json", http.StatusInternalServerError)
 		}
-
-		w.Write(item)
+		w.Write(resp)
 		return
 	}
 
 	fmt.Fprintf(w, "Hello, world")
 }
 
-func getResponse() map[string]interface{} {
-	test := make(map[string]interface{})
-	const RESP = "THIS IS FINALLY WORKING"
-	test["speech"] = RESP
-	test["displayText"] = RESP
-	return test
+func buildFulfillment(user *User) *FulfillmentResp {
+	summary := fmt.Sprintf(
+		"Hello %s. You currently have %d public repos, "+
+			"%d private repos, and you own %d of these private repos."+
+			"You have %d followers and are following %d people.",
+		user.Name, user.PublicRepos, user.TotalPrivateRepos,
+		user.OwnedPrivateRepos, user.Followers, user.Following)
+	resp := &FulfillmentResp{Speech: summary, DisplayText: summary}
+	return resp
+}
+
+func getCurrentUser(accessToken string) (*User, error) {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", "https://api.github.com/user", nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Authorization", "token "+accessToken)
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	decoder := json.NewDecoder(resp.Body)
+	user := &User{}
+
+	err = decoder.Decode(user)
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
 }
 
 func main() {
